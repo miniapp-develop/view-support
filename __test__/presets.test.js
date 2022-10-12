462776c6c1e5bbc2bc4cb6d4eae6b7a6431911af
xesam <xesam@outlook.com>
2022-10-12 21:28:47 +0800

    presets /test

--- __test__/presets.test.js
+++ __test__/presets.test.js
@@ -1,27 +1,22 @@
 const presets = require('../lib/presets');
 
-
 describe('presets', () => {
     beforeEach(() => {
         global.Page = jest.fn();
     });
-    test('preset Page', () => {
+    test('preset Page with nothing', () => {
+        const NewPage = presets.Page();
+        NewPage();
+        expect(Page).toBeCalledTimes(1);
+    });
+    test('preset Page with option', () => {
         const NewPage = presets.Page({
             name: 'preset-value'
         });
         NewPage();
         expect(Page).toBeCalledTimes(1);
     });
-    test('use last value', () => {
-        const NewPage = presets.Page({
-            name: 'preset-value'
-        });
-        NewPage({
-            name: 'newest-value'
-        });
-        expect(Page.mock.calls[0][0].name).toEqual('newest-value');
-    });
-    test('merge option attrs', () => {
+    test('preset Page with option then merge option attrs', () => {
         const NewPage = presets.Page({
             name1: 'preset-value'
         });
@@ -30,5 +25,14 @@ describe('presets', () => {
         });
         expect(Page.mock.calls[0][0].name1).toEqual('preset-value');
         expect(Page.mock.calls[0][0].name2).toEqual('newest-value');
+    });
+    test('preset Page with option then override same key value', () => {
+        const NewPage = presets.Page({
+            name: 'preset-value'
+        });
+        NewPage({
+            name: 'newest-value'
+        });
+        expect(Page.mock.calls[0][0].name).toEqual('newest-value');
     });
 });
