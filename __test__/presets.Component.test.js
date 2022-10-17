4edb7484a9cc5234315f9246f628f0b0ef586ca0
xesam <xesamguo@gmail.com>
2022-10-17 16:07:38 +0800

    presets Component test /update

--- __test__/presets.Component.test.js
+++ __test__/presets.Component.test.js
@@ -1,29 +1,21 @@
 const presets = require('../lib/presets');
 
-describe('presets Page', () => {
+describe('presets Component', () => {
     beforeEach(() => {
-        global.Page = jest.fn();
-        global.Behavior = () => {
-            const fn = jest.fn();
-            fn.mockReturnValue('/a/bcdef')
-            return fn();
-        };
+        global.Component = jest.fn();
     });
-    test('when preset with nothing then apply undefined', () => {
-        const NewPage = presets.Page();
-        NewPage();
-        expect(Page).toBeCalledTimes(1);
-        expect(Page.mock.calls[0][0].name).toBeUndefined();
-    });
-    test('when preset with option then apply preset option', () => {
-        const NewPage = presets.Page({
-            name: 'preset-value'
+    test('when preset with option then merge keys', () => {
+        const NewPage = presets.Component({
+            name1: 'preset-value'
         });
-        NewPage();
-        expect(Page.mock.calls[0][0].name).toEqual('preset-value');
+        NewPage({
+            name2: 'newest-value'
+        });
+        expect(Component.mock.calls[0][0].name1).toEqual('preset-value');
+        expect(Component.mock.calls[0][0].name2).toEqual('newest-value');
     });
     test('when preset with option then merge option.data', () => {
-        const NewPage = presets.Page({
+        const NewPage = presets.Component({
             data: {
                 name1: 'preset-value'
             }
@@ -33,13 +25,13 @@ describe('presets Page', () => {
                 name2: 'newest-value'
             }
         });
-        expect(Page.mock.calls[0][0].data).toStrictEqual({
+        expect(Component.mock.calls[0][0].data).toStrictEqual({
             name1: 'preset-value',
             name2: 'newest-value'
         });
     });
     test('when preset with option then merge option.options', () => {
-        const NewPage = presets.Page({
+        const NewPage = presets.Component({
             options: {
                 name1: 'preset-value'
             }
@@ -49,22 +41,22 @@ describe('presets Page', () => {
                 name2: 'newest-value'
             }
         });
-        expect(Page.mock.calls[0][0].options).toStrictEqual({
+        expect(Component.mock.calls[0][0].options).toStrictEqual({
             name1: 'preset-value',
             name2: 'newest-value'
         });
     });
     test('when preset with option then concat option.behaviors', () => {
-        const NewPage = presets.Page({
+        const NewPage = presets.Component({
             behaviors: [1]
         });
         NewPage({
             behaviors: [2]
         });
-        expect(Page.mock.calls[0][0].behaviors).toStrictEqual([1, 2]);
+        expect(Component.mock.calls[0][0].behaviors).toStrictEqual([1, 2]);
     });
     test('when preset with a single option then merge keys', () => {
-        const NewPage = presets.Page({
+        const NewPage = presets.Component({
             name: 'preset-value',
             name1: 'preset-value'
         });
@@ -72,8 +64,8 @@ describe('presets Page', () => {
             name: 'newest-value',
             name2: 'newest-value'
         });
-        expect(Page.mock.calls[0][0].name1).toEqual('preset-value');
-        expect(Page.mock.calls[0][0].name2).toEqual('newest-value');
+        expect(Component.mock.calls[0][0].name1).toEqual('preset-value');
+        expect(Component.mock.calls[0][0].name2).toEqual('newest-value');
     });
     test('when preset with multi options then merge keys', () => {
         const option1 = {
@@ -86,98 +78,98 @@ describe('presets Page', () => {
             sameName1: 'option2-same-name-2',
             sameName2: 'option2-same-name-2'
         };
-        const NewPage = presets.Page(option1, option2);
+        const NewPage = presets.Component(option1, option2);
         NewPage({
             newName: 'newest-value',
             sameName2: 'newest-same-name-2',
             sameName3: 'newest-same-name-3'
         });
-        expect(Page.mock.calls[0][0].presetName1).toEqual('option1-value-1');
-        expect(Page.mock.calls[0][0].presetName2).toEqual('option2-value-2');
-        expect(Page.mock.calls[0][0].newName).toEqual('newest-value');
-        expect(Page.mock.calls[0][0].sameName1).toEqual('option2-same-name-2');
-        expect(Page.mock.calls[0][0].sameName2).toEqual('newest-same-name-2');
-        expect(Page.mock.calls[0][0].sameName3).toEqual('newest-same-name-3');
+        expect(Component.mock.calls[0][0].presetName1).toEqual('option1-value-1');
+        expect(Component.mock.calls[0][0].presetName2).toEqual('option2-value-2');
+        expect(Component.mock.calls[0][0].newName).toEqual('newest-value');
+        expect(Component.mock.calls[0][0].sameName1).toEqual('option2-same-name-2');
+        expect(Component.mock.calls[0][0].sameName2).toEqual('newest-same-name-2');
+        expect(Component.mock.calls[0][0].sameName3).toEqual('newest-same-name-3');
     });
     test('when preset with a single factory then merge keys', () => {
-        const aOldPageFactory = presets.Page({
+        const aOldPageFactory = presets.Component({
             presetName: 'old-factory-value',
             sameName: 'same-old-factory-value'
         });
-        const NewPage = presets.Page(aOldPageFactory);
+        const NewPage = presets.Component(aOldPageFactory);
         NewPage({
             newName: 'new-value',
             sameName: 'same-new-value'
         });
-        expect(Page.mock.calls[0][0].sameName).toEqual('same-new-value');
+        expect(Component.mock.calls[0][0].sameName).toEqual('same-new-value');
     });
     test('when preset with multi factories then merge keys', () => {
-        const factory1 = presets.Page({
+        const factory1 = presets.Component({
             presetName1: 'factory1-value-1',
             sameName1: 'factory1-same-name-1',
             sameName3: 'factory1-same-name-3'
         });
-        const factory2 = presets.Page({
+        const factory2 = presets.Component({
             presetName2: 'factory2-value-2',
             sameName1: 'factory2-same-name-2',
             sameName2: 'factory2-same-name-2'
         });
-        const NewPage = presets.Page(factory1, factory2);
+        const NewPage = presets.Component(factory1, factory2);
         NewPage({
             newName: 'newest-value',
             sameName2: 'newest-same-name-2',
             sameName3: 'newest-same-name-3'
         });
-        expect(Page.mock.calls[0][0].presetName1).toEqual('factory1-value-1');
-        expect(Page.mock.calls[0][0].presetName2).toEqual('factory2-value-2');
-        expect(Page.mock.calls[0][0].newName).toEqual('newest-value');
-        expect(Page.mock.calls[0][0].sameName1).toEqual('factory2-same-name-2');
-        expect(Page.mock.calls[0][0].sameName2).toEqual('newest-same-name-2');
-        expect(Page.mock.calls[0][0].sameName3).toEqual('newest-same-name-3');
+        expect(Component.mock.calls[0][0].presetName1).toEqual('factory1-value-1');
+        expect(Component.mock.calls[0][0].presetName2).toEqual('factory2-value-2');
+        expect(Component.mock.calls[0][0].newName).toEqual('newest-value');
+        expect(Component.mock.calls[0][0].sameName1).toEqual('factory2-same-name-2');
+        expect(Component.mock.calls[0][0].sameName2).toEqual('newest-same-name-2');
+        expect(Component.mock.calls[0][0].sameName3).toEqual('newest-same-name-3');
     });
     test('when preset with factory then merge option.data', () => {
-        const aOldPageFactory = presets.Page({
+        const aOldPageFactory = presets.Component({
             data: {
                 presetName: 'old-factory-value',
                 sameName: 'same-old-factory-value'
             }
         });
-        const NewPage = presets.Page(aOldPageFactory);
+        const NewPage = presets.Component(aOldPageFactory);
         NewPage({
             data: {
                 newName: 'new-value',
                 sameName: 'same-new-value'
             }
         });
-        expect(Page.mock.calls[0][0].data).toStrictEqual({
+        expect(Component.mock.calls[0][0].data).toStrictEqual({
             presetName: 'old-factory-value',
             newName: 'new-value',
             sameName: 'same-new-value'
         });
     });
     test('when preset with multi factories then merge option.data', () => {
-        const factory1 = presets.Page({
+        const factory1 = presets.Component({
             data: {
                 presetName1: 'factory1-value-1',
                 sameName1: 'factory1-same-name-1',
                 sameName3: 'factory1-same-name-3'
             }
         });
-        const factory2 = presets.Page({
+        const factory2 = presets.Component({
             data: {
                 presetName2: 'factory2-value-2',
                 sameName1: 'factory2-same-name-2',
                 sameName2: 'factory2-same-name-2'
             }
         });
-        const factory3 = presets.Page({
+        const factory3 = presets.Component({
             data: {
                 presetName3: 'factory3-value-3',
                 sameName1: 'factory3-same-name-3',
                 sameName2: 'factory3-same-name-3'
             }
         });
-        const NewPage = presets.Page(factory1, factory2, factory3);
+        const NewPage = presets.Component(factory1, factory2, factory3);
         NewPage({
             data: {
                 newName: 'newest-value',
@@ -185,7 +177,7 @@ describe('presets Page', () => {
                 sameName3: 'newest-same-name-3'
             }
         });
-        expect(Page.mock.calls[0][0].data).toStrictEqual({
+        expect(Component.mock.calls[0][0].data).toStrictEqual({
             presetName1: 'factory1-value-1',
             presetName2: 'factory2-value-2',
             presetName3: 'factory3-value-3',
@@ -201,23 +193,23 @@ describe('presets Page', () => {
             sameName1: 'factory1-same-name-1',
             sameName3: 'factory1-same-name-3'
         };
-        const option2 = presets.Page({
+        const option2 = presets.Component({
             presetName3: 'factory3-value-3',
             sameName1: 'factory3-same-name-3',
             sameName2: 'factory3-same-name-3'
         });
-        const NewPage = presets.Page(option1, option2);
+        const NewPage = presets.Component(option1, option2);
         NewPage({
             newName: 'newest-value',
             sameName2: 'newest-same-name-2',
             sameName3: 'newest-same-name-3'
         });
 
-        expect(Page.mock.calls[0][0].presetName1).toEqual('factory1-value-1');
-        expect(Page.mock.calls[0][0].presetName3).toEqual('factory3-value-3');
-        expect(Page.mock.calls[0][0].newName).toEqual('newest-value');
-        expect(Page.mock.calls[0][0].sameName1).toEqual('factory3-same-name-3');
-        expect(Page.mock.calls[0][0].sameName2).toEqual('newest-same-name-2');
-        expect(Page.mock.calls[0][0].sameName3).toEqual('newest-same-name-3');
+        expect(Component.mock.calls[0][0].presetName1).toEqual('factory1-value-1');
+        expect(Component.mock.calls[0][0].presetName3).toEqual('factory3-value-3');
+        expect(Component.mock.calls[0][0].newName).toEqual('newest-value');
+        expect(Component.mock.calls[0][0].sameName1).toEqual('factory3-same-name-3');
+        expect(Component.mock.calls[0][0].sameName2).toEqual('newest-same-name-2');
+        expect(Component.mock.calls[0][0].sameName3).toEqual('newest-same-name-3');
     });
 });
