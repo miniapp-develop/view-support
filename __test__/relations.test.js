ef7227b8683db7cc517543d657e9cd7ceceab333
xesam <xesamguo@gmail.com>
2022-10-18 10:12:50 +0800

    relations test /update

--- __test__/relations.test.js
+++ __test__/relations.test.js
@@ -3,12 +3,14 @@ const relation = require('../lib/relations');
 global.Behavior = jest.fn();
 
 describe('relation', () => {
-    test('simple check', () => {
+    test('simple check createParentChild', () => {
         const {parent, child} = relation.createParentChild();
         expect(parent).not.toBeNull();
         expect(child).not.toBeNull();
     });
+    test('simple check createAncestorDescendant', () => {
+        const {ancestor, descendant} = relation.createAncestorDescendant();
+        expect(ancestor).not.toBeNull();
+        expect(descendant).not.toBeNull();
+    });
 });
-
-// const ParentView = relation.mixin();
-// const ChildView = relation.mixin();
