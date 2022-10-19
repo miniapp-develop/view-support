0d4c820bbb26d996a314f7f148d7b20f3496654b
xesam <xesamguo@gmail.com>
2022-10-19 11:52:38 +0800

    merges /fix

--- __test__/merges.test.js
+++ __test__/merges.test.js
@@ -52,7 +52,55 @@ describe('mergePageOption', () => {
             'preset-options-key': 'preset-options-value'
         });
         expect(mergedOption.__attr__).toBe('a-string');
-        expect(mergedOption.behaviors.length).toBe(2);
+        expect(mergedOption.behaviors).toStrictEqual(['a', '/a/b/c/d/e/f']);
+        expect(global.Behavior.mock.calls[0][0].data).toStrictEqual({
+            'preset-data-key': 'preset-data-value'
+        });
+        expect(Object.keys(global.Behavior.mock.calls[0][0].pageLifetimes)).toEqual(['show', 'hide', 'resize']);
+        expect(Object.keys(global.Behavior.mock.calls[0][0].lifetimes)).toEqual(['ready']);
+        expect(Object.keys(global.Behavior.mock.calls[0][0].methods)).toEqual(['onLoad', 'onUnload', 'pageMethod']);
+    });
+
+    test('when inputOption has BehaviorOwnProperty then wrap a new Behavior', () => {
+        const presetOption = null;
+        const inputOption = {
+            options: {
+                'preset-options-key': 'preset-options-value'
+            },
+            data: {
+                'preset-data-key': 'preset-data-value'
+            },
+            behaviors: ['a'],
+            onLoad(query) {
+                console.log('Page.onLoad');
+            },
+            onShow() {
+                console.log('Page.onShow');
+            },
+            onReady() {
+                console.log('Page.onReady');
+            },
+            onHide() {
+                console.log('Page.onHide');
+            },
+            onUnload() {
+                console.log('Page.onUnload');
+            },
+            onResize() {
+                console.log('Page.onResize');
+            },
+            pageMethod() {
+                console.log('Page.pageMethod');
+            },
+            __attr__: 'a-string'
+        };
+        const mergedOption = merges.mergePageOption(presetOption, inputOption);
+        expect(global.Behavior).toBeCalled();
+        expect(mergedOption.options).toStrictEqual({
+            'preset-options-key': 'preset-options-value'
+        });
+        expect(mergedOption.__attr__).toBe('a-string');
+        expect(mergedOption.behaviors).toStrictEqual(['a', '/a/b/c/d/e/f']);
         expect(global.Behavior.mock.calls[0][0].data).toStrictEqual({
             'preset-data-key': 'preset-data-value'
         });
