5b3c5c6e7950281446084f38edc19b42d2a7f0a9
xesam <xesamguo@gmail.com>
2022-10-27 11:55:22 +0800

    relations /fix

--- __test__/relations.test.js
+++ __test__/relations.test.js
@@ -1,9 +1,11 @@
 const relation = require('../lib/relations');
 
-global.Behavior = jest.fn();
-global.Component = jest.fn();
-
 describe('relation', () => {
+    beforeEach(() => {
+        global.Page = jest.fn();
+        global.Component = jest.fn();
+        global.Behavior = jest.fn().mockReturnValue('1');
+    });
     test('simple check createParentChild', () => {
         const {parent, child} = relation.createParentChild();
         expect(parent).toBeInstanceOf(Function);
@@ -13,5 +15,10 @@ describe('relation', () => {
         const {ancestor, descendant} = relation.createAncestorDescendant();
         expect(ancestor).toBeInstanceOf(Function);
         expect(descendant).toBeInstanceOf(Function);
+    });
+    test('when create with config then return relation-behaviors', () => {
+        const {parent, children} = relation.createRelation('form', ['input', "button", "picker"]);
+        expect(parent).toEqual('1');
+        expect(children).toEqual(['1', '1', '1']);
     });
 });
